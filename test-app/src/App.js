d3fa427de4b4af86d262015a67ddeff3de774b25
souravbaranwal <sourav.baranwal@gmail.com>
2019-06-12 13:58:38 +0530

    updated mini projects

---
+++ test-app/src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./App.scss";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 'efer', name: "Sourav", age: 30 },
+      {id: 'thlt', name: "Soniya", age: 28 },
+      {id: 'ltph', name: "Sushma", age: 55 }
+    ],
+    otherState: "test state",
+    showPersons: false
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  nameChangeHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 26 }
+      ]
+    });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id} />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersonHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
