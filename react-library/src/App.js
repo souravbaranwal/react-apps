fb6b83df1e41a821653b96ab1a0882d7dc0ca9e0
souravbaranwal <sourav.baranwal@gmail.com>
2019-06-28 10:17:40 +0530

    update button component with label, type & size

--- react-library/src/App.js
+++ react-library/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './App.scss';
-import Button from './Components/Button';
+import React from "react";
+import "./App.scss";
+import Button from "./Components/Button";
 
 function App() {
   return (
-   <>
-    <Button label = 'Home' type= 'primary'/>
-    <Button label = 'Contact Us' type= 'secondary'/>
-    <Button label = 'About Us' type= 'primary' size= 'small'/>
-    <Button/>
-   </>
+    <>
+      <Button label="Home" type="link" />
+      <Button label="Contact Us" type="secondary" />
+      <Button label="About Us" type="info" size="small" />
+      <Button
+        label="About Us"
+        type="info"
+        size="large"
+        onClickHandler={() => alert("You CLicked Me!")}
+      />
+      <Button />
+    </>
   );
 }
 
--- react-library/src/Components/Button.js
+++ react-library/src/Components/Button.js
@@ -1,25 +1,72 @@
-import React, { Component } from 'react';
-import Proptypes from 'prop-types';
-
+import React, { Component } from "react";
+// import Proptypes from 'prop-types';
+import "./CSS/button.scss";
 
 class Button extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleStyle = () => {
+    let { size, type } = this.props;
+    let style = {};
+
+    // color code
+
+    switch (type) {
+      case "primary":
+        style = { background: "blue" };
+        break;
+      case "danger":
+        style = { background: "##F5F5F5" };
+        break;
+      case "success":
+        style = { background: "#23d160" };
+        break;
+      case "info":
+        style = { background: "#209cee" };
+        break;
+      case "link":
+        style = { background: "#3273dc" };
+        break;
+      case "small":
+        style = { fontSize: "12px" };
+        break;
+      default:
+        style = { background: "#555555" };
     }
-    render() {
-        return (
-            <button>
-                {this.props.label || 'please add button name'}
-            </button>
-        )
+    // size code
+
+    switch (size) {
+      case "small":
+        style = { ...style, fontSize: "12px" };
+        break;
+      case "large":
+        style = { ...style, fontSize: "20px" };
+        break;
+
+      default:
+        style = { ...style, fontSize: "16px" };
     }
+
+    return style;
+  };
+  render() {
+    let { label } = this.props;
+
+    return (
+      <button style={this.handleStyle()} onClick={this.props.onClickHandler}>
+        {label || "please add button name"}
+      </button>
+    );
+  }
 }
 
-Button.propTypes = {
-    label: Proptypes.string.isRequired,
-    type: Proptypes.string.isRequired,
-    size: Proptypes.string.isRequired,
-}
+// Button.propTypes = {
+//     label: Proptypes.string.isRequired,
+//     type: Proptypes.string.isRequired,
+//     size: Proptypes.string.isRequired,
+// }
 
 export default Button;
